b4b7215644596c5162f9cd43ff891cc832412f52
primesim1 <140283586+primesim1@users.noreply.github.com>
2024-04-23 19:22:02 -0400

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<cassert>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+      vector<int> res;
+      for (int n: nums) {
+        if (n!=val) {
+            res.push_back(n);
+        }
+      }  
+      int count=0;
+      for (int n:res) {
+        nums[count++] = n;
+      }
+      return count;
+    }
+};
+int main() {
+    vector<int> arr{2,1,7,4,3,8,3,1};
+    Solution soln;
+    int count = soln.removeElement(arr,3);
+    assert(count==6);
+
+    return 0;
+}
